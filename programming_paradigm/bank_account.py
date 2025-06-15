6978f2748cf4486168b0039c27a1a75292275949
Ndata <wisdomndata@gmail.com>
2025-06-15 20:14:50 +0100

    New

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, initial_balance=0.0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current balance: ${self.__account_balance:.2f}")
