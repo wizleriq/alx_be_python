a7ce2e598eeebc0387c8fd6a8e5ce1f8cb115a2a
Ndata <wisdomndata@gmail.com>
2025-06-15 20:19:07 +0100

    New

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,7 +15,7 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: {self.__account_balance}")
+        print(f"Current Balance: ${self.__account_balance:.2f}")
 
 
 
