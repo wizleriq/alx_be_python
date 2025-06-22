b73cd27e5bf8dec08a6b8b93201a73d1992000c5
Ndata <wisdomndata@gmail.com>
2025-06-22 22:20:33 +0100

    OOP

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,11 +1,11 @@
 class Calculator:
-    calculation_type  = "Arithmetic Operations"
+    calculation_type = "Arithmetic Operations"
 
     @staticmethod
-    def add (a, b):
+    def add(a, b):
         return a + b
-    
+
     @classmethod
     def multiply(cls, a, b):
-        print(f"Calcualtion type: {cls.Calculation_type}")
+        print(f"Calculation type: {cls.calculation_type}")
         return a * b
